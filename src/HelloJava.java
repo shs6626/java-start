28fd2d00065c6cba9cfddde57af44dfd3557d5f1
Hongunner <shs6626@naver.com>
2024-04-03 22:20:55 +0900

    JAVA START

---
+++ src/HelloJava.java
@@ -0,0 +1,15 @@
+public class HelloJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        int arsenal[];
+        arsenal = new int[5];
+        arsenal[0] = 1;
+
+        System.out.println(arsenal);
+        System.out.println(arsenal[0]);
+        System.out.println(arsenal[1]);
+
+    }
+}
